2f3fe92c64db06efe87034b53f36b876e9374531
gysong <gysong817@gmail.com>
2024-01-23 16:27:26 +0900

    mainController add method
    2024-01-23 16:27

--- Team3_SemiFinal_MiniProject/src/main/java/com/miniproject/controller/MainController.java
+++ Team3_SemiFinal_MiniProject/src/main/java/com/miniproject/controller/MainController.java
@@ -1,5 +1,28 @@
 package com.miniproject.controller;
 
-public class MainController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.miniproject.dto.BoardDTO;
+import com.miniproject.service.BoardService;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	private BoardService service;
+	
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String getAllBoard(Model model) {
+		
+		List<BoardDTO> boardList = service.getAllBoard();
+		model.addAttribute("boardList", boardList);
+		
+		return "main";
+	}
 }
